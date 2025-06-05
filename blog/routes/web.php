062705035cbcb82e76bd0fe52b31593817aa422f
Shakk1N <159267166+Shakk1N@users.noreply.github.com>
2025-06-05 13:32:51 +0300

    Lab 3
    Added RestTestController and its routes. Created Blog/BaseController and Blog/PostController with REST support. Configured routes for blog/posts with 'blog' prefix. Created index.blade.php template for displaying posts and main.blade.php base template. Added Soft Deletes support to BlogPost model.

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,16 +1,24 @@
 <?php
 
+use App\Http\Controllers\RestTestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('rest', RestTestController::class)->names('restTest');
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+    Route::group(['namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+        Route::resource('posts', PostController::class)->names('blog.posts');
+    });
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
