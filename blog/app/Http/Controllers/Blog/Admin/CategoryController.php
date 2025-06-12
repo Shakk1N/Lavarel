eb719be633bb74daa59c23b56cbff7f4f03bbe18
Shakk1N <159267166+Shakk1N@users.noreply.github.com>
2025-06-12 17:23:41 +0300

    Lab 6
    Лабораторна 6. Створення репозиторія та оптимізація запитів

--- blog/app/Http/Controllers/Blog/Admin/CategoryController.php
+++ blog/app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,115 +2,100 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
-// use App\Http\Controllers\Controller;
 use App\Models\BlogCategory;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Requests\BlogCategoryCreateRequest;
 use App\Http\Requests\BlogCategoryUpdateRequest;
-
+use App\Repositories\BlogCategoryRepository;
 
 class CategoryController extends BaseController
 {
     /**
-     * Display a listing of the resource.
+     * @var BlogCategoryRepository
      */
-   public function index()
-{
-    $paginator = BlogCategory::paginate(5);
-    return view('blog.admin.categories.index', compact('paginator'));
-}
+    private $blogCategoryRepository;
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function __construct()
     {
-    $item = new BlogCategory();
-    $categoryList = BlogCategory::all();
-
-    return view('blog.admin.categories.edit', compact('item', 'categoryList'));
-
+        // йіцвйцв
+        $this->blogCategoryRepository = app(BlogCategoryRepository::class);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    public function index()
+    {
+        $paginator = $this->blogCategoryRepository->getAllWithPaginate(5);
+        return view('blog.admin.categories.index', compact('paginator'));
+    }
+
+    public function create()
+    {
+        $item = new BlogCategory();
+        $categoryList = $this->blogCategoryRepository->getForComboBox();
+
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
+    }
+
     public function store(BlogCategoryCreateRequest $request)
     {
         $data = $request->input();
 
-    if (empty($data['slug'])) {
-    $data['slug'] = Str::slug($data['title']);
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+        $item = (new BlogCategory())->create($data);
+
+        if ($item) {
+            return redirect()
+                ->route('blog.admin.categories.edit', [$item->id])
+                ->with(['success' => 'Успішно збережено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Помилка збереження'])
+                ->withInput();
+        }
     }
 
-    $item = (new BlogCategory())->create($data);
-
-    if ($item) {
-    return redirect()
-        ->route('blog.admin.categories.edit', [$item->id])
-        ->with(['success' => 'Успішно збережено']);
-} else {
-    return back()
-        ->withErrors(['msg' => 'Помилка збереження'])
-        ->withInput();
-}
-
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit($id)
-{
-    $item = BlogCategory::findOrFail($id);
-    $categoryList = BlogCategory::all();
-    return view('blog.admin.categories.edit', compact('item', 'categoryList'));
-}
+    {
+        $item = $this->blogCategoryRepository->getEdit($id);
+        if (empty($item)) {
+            abort(404);
+        }
 
-    /**
-     * Update the specified resource in storage.
-     */
+        $categoryList = $this->blogCategoryRepository->getForComboBox($item->parent_id);
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
+    }
+
     public function update(BlogCategoryUpdateRequest $request, $id)
-{
-    $item = BlogCategory::find($id);
-    if (empty($item)) {
-        return back()
-            ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"])
-            ->withInput();
+    {
+        $item = $this->blogCategoryRepository->getEdit($id);
+        if (empty($item)) {
+            return back()
+                ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"])
+                ->withInput();
+        }
+
+        $data = $request->all();
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+        $result = $item->update($data);
+
+        if ($result) {
+            return redirect()
+                ->route('blog.admin.categories.edit', $item->id)
+                ->with(['success' => 'Успішно збережено']);
+        } else {
+            return back()
+                ->with(['msg' => 'Помилка збереження'])
+                ->withInput();
+        }
     }
-
-    $data = $request->all();
-    if (empty($data['slug'])) {
-        $data['slug'] = Str::slug($data['title']);
-    }
-
-    $result = $item->update($data);
-
-    if ($result) {
-        return redirect()
-            ->route('blog.admin.categories.edit', $item->id)
-            ->with(['success' => 'Успішно збережено']);
-    } else {
-        return back()
-            ->with(['msg' => 'Помилка збереження'])
-            ->withInput();
-    }
-}
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+
     }
 }
---
+++ blog/app/Repositories/BlogCategoryRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogCategory as Model;
+use Illuminate\Database\Eloquent\Collection;
+
+class BlogCategoryRepository extends CoreRepository
+{
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+    public function getEdit($id)
+    {
+        return $this->startConditions()->find($id);
+    }
+
+    public function getForComboBox()
+    {
+        $columns = implode(', ', [
+            'id',
+            'CONCAT (id, ". ", title) AS id_title',
+        ]);
+
+        return $this
+            ->startConditions()
+            ->selectRaw($columns)
+            ->toBase()
+            ->get();
+    }
+
+    public function getAllWithPaginate($perPage = null)
+    {
+        $columns = ['id', 'title', 'parent_id'];
+
+        return $this
+            ->startConditions()
+            ->select($columns)
+            ->paginate($perPage);
+    }
+}
