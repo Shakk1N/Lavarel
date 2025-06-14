71b47d4555000d845674aaadf71334739895c925
Shakk1N <159267166+Shakk1N@users.noreply.github.com>
2025-06-14 23:13:55 +0300

    Lab 10
    Лабораторна 10. Додавання і видалення статей

--- blog/app/Repositories/BlogCategoryRepository.php
+++ blog/app/Repositories/BlogCategoryRepository.php
@@ -38,6 +38,7 @@ class BlogCategoryRepository extends CoreRepository
         return $this
             ->startConditions()
             ->select($columns)
+            ->with(['parentCategory:id,title',])
             ->paginate($perPage);
     }
 }
